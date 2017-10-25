6f03bf831b9e61bb16817f7c37f7bbd7973d6c26
MNDavey <uqmdave5@uq.edu.au>
2017-10-25 14:43:06 +1000

    inital commit (commiting is a holder in work for when you fuck up)

---
+++ R/praiseme.r
@@ -0,0 +1,10 @@
+#' Gives praise
+#'
+#'you this to give a dscription : gives praise in times of need
+#' @return
+#' @export #leave the export always this means i can find it in the future
+#'
+#' @examples
+praiseme <- function(){
+  print("You are awesome")
+}
